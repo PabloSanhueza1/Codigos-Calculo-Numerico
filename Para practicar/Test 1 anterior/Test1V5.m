98758d2915bb14bf60eee98ee9df29c750973ac1
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-09-22 18:11:08 -0300

    commit

--- Para practicar/Test 1 anterior/Test1V5.m
+++ Para practicar/Test 1 anterior/Test1V5.m
@@ -7,11 +7,19 @@ cota = simpson2(0,1,1*10^-12);
 
 while dif <= 1*10^-12
     n = n + 1;
-    if simpson1(1/2,1,n) < cota
-        I1 = simpson1(1/2,1,n);
-        if simpson1(1/2,1,n-1) < cota
-            I2 = simpson1(1/2,1,n-1);
+    if simpson1(1/n,1,n) < cota
+
+        I1 = simpson1(1/n,1,n);
+
+        if simpson1(1/n,1,n-1) < cota
+
+            I2 = simpson1(1/n,1,n-1);
+            
             dif = abs(I1 - I2);
         end
     end
 end
+
+fprintf("Con un n: %d\n",n);
+fprintf("Se tiene un f_%d tal que\n",n);
+fprintf("f_%d: %f\n",n,I1);
