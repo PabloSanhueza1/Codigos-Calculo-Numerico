5b9258a8e517822fcf3216d8d6112f1165c3e397
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-10-04 18:09:19 -0300

    metodo de euler explicito

--- Codigos utiles/EDO/euler.m
+++ Codigos utiles/EDO/euler.m
@@ -1,14 +1,15 @@
 function [x,y] = euler(y0,a,b,f,n)
 
-h = (b-a)/2;
+h = (b-a)/n;
 x = [];
 y = [];
 
 x(1) = a;
 y(1) = y0;
 
-for i = 1:n
+for i = 1: n
     x(i+1) = x(i) + h;
     y(i+1) = y(i) + h * f(x(i),y(i));
 end
+
 end
