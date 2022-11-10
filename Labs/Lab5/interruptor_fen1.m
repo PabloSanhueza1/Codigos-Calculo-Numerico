95f9bc8257af87477bdb91ff77761589c23a2a2b
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-11-10 13:26:37 -0300

    lab8

--- Labs/Lab5/interruptor_fen1.m
+++ Labs/Lab5/interruptor_fen1.m
@@ -3,7 +3,7 @@ clc
 
 %umbral entre 0.1 - 0.4
 s = [0.1 0.2 0.3 0.4];
-colores = {'b','r','k','g'};
+colores = ["b","r","k","g"];
 g0 = 0;
 
 for i = 1: length(s)
@@ -18,7 +18,7 @@ legend("0.1","0.2","0.3","0.4")
 
 %umbral entre 0.201 - 0.204
 s = [0.201 0.202 0.203 0.204];
-colores = {'b','r','k','g'};
+colores = ["b","r","k","g"];
 g0 = 0;
 
 for i = 1: length(s)
--- Labs/Lab6/Actividad1.m
+++ Labs/Lab6/Actividad1.m
@@ -4,32 +4,39 @@ clc
 f = @(t,x) -3*t*x^2 + 1/(1+t^3);
 a = 0; b = 5; x0 = 0;
 
-[t1,x1] = euler(x0,a,b,f,10);
-[t2,x2] = euler(x0,a,b,f,20);
-[t3,x3] = euler(x0,a,b,f,40);
-[t4,x4] = euler(x0,a,b,f,80);
+[t1,x1] = eulerExplicito(x0,a,b,f,10);
+[t2,x2] = eulerExplicito(x0,a,b,f,20);
+[t3,x3] = eulerExplicito(x0,a,b,f,40);
+[t4,x4] = eulerExplicito(x0,a,b,f,80);
 
 plot(t1,x1,'r')
 hold on
 plot(t2,x2,'b')
 plot(t3,x3,'k')
 plot(t4,x4,'g')
+legend("n = 10", "n = 20", "n = 40", "n = 80")
 
-j = 0:10;
-x21 = x2(2*j+1);
-[m1,id1] = max(abs(x1-x21))
+
+%aproximacion de x2 a 11 elementos
+i = 0:10;
+t21 = t2(2*i+1);
+x21 = x2(2*i+1);
+[m1,id1] = max(abs(x1-x21));
 t1(id1)
 
-j = 0:20;
-x32 = x3(2*j+1);
-[m2,id2] = max(abs(x2-x32))
+%aproximacion de x3 a 21 elementos
+i = 0:20;
+t32 = t3(2*i+1);
+x32 = x3(2*i+1);
+[m2,id2] = max(abs(x2-x32));
 t2(id2)
 
-j = 0:40;
-x43 = x4(2*j+1);
-[m3,id3] = max(abs(x3-x43))
+%aproximacion de x3 a 41 elementos
+i = 0:40;
+t43 = t4(2*i+1);
+x43 = x4(2*i+1);
+[m3,id3] = max(abs(x3-x43));
 t3(id3)
-
 
 aux = @(t) t/(1+t^3);
 xx = linspace(a,b,81);
@@ -39,8 +46,16 @@ for i=1:81
     yy(i) = aux(xx(i));
 end
 
-for i=1:81
+for i = 1: 81
     exacta(i) = aux(t4(i));
 end
 
 error = max(abs(exacta - x4));
+%aproximacion de x2 a 11 elementos
+for i = 0: 10
+    t21 = t2(2*i+1);
+    x21 = x2(2*i+1);
+end
+
+
+
