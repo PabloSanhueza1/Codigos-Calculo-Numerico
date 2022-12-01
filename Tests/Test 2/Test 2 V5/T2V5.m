0593a2f281a6849c24602e04508b3f601ebb26a7
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-12-01 18:31:52 -0300

    commit

--- Tests/Test 2/Test 2 V5/T2V5.m
+++ Tests/Test 2/Test 2 V5/T2V5.m
@@ -1,6 +1,8 @@
 clear
 clc
 
+% NOTA: Se utiliza la letra "a" para referirse a "alpha"
+
 F = [0 1 2 3 4 5 6 7 8 9 10];
 P = [5.03 4.71 4.4 3.97 3.88 3.62 3.3 3.15 3.08 2.92 2.7];
 P = P*10^-4;
@@ -27,6 +29,8 @@ A = [ones(size(F))', F'];
 x = (A'*A)\(A'*b);
 n0 = x(1);
 a = (x(2)/n0)*-1;
+fprintf("Valor de n0 = %f\n",n0)
+fprintf("Valor de a (alpha) = %f\n",a)
 
 %- item d -%
 M = @(t) n0-n0*a*t;
@@ -36,6 +40,7 @@ legend("funcion obtenida","datos de tabla")
 
 %- item e -%
 prediccion = M(F(end)+2);
+fprintf("\n")
 fprintf("La concentracion de iones a los 12s sera de %f\n", prediccion)
 fprintf("Note que, a diferencia de la tabla, este resultado ya esta\n")
 fprintf("multiplicado por 10^-4\n")
